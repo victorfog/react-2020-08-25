7cccb9f512d53951f6aa204da098296c93d6f58e
Andrii Koretskyi <andriiko@wix.com>
2020-09-10 17:07:28 +0300

    move restaurants to redux

--- src/redux/reducer/index.js
+++ src/redux/reducer/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
-import orderReducer from './order';
+
+import order from './order';
+import restaurants from './restaurants';
 
 export default combineReducers({
-  foo: () => 'bar',
-  order: orderReducer,
+  order,
+  restaurants,
 });
